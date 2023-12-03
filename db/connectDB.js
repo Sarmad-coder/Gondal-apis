17a7c155cfd97f021dd553c9a7063c46dbc11c24
Sarmad-coder <sarmadawan35@gmail.com>
2023-12-04 01:57:07 +0500

    secureDB .env file created

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://sarmadawan35:DtNplVTmaWuQdS9S@mycluster.cytjjkg.mongodb.net/gondalDb");
+    await mongoose.connect(process.env.DATABASE_URL);
     return console.log("DB connected...");
   } catch (error) {
     return console.log(`DB Connection Error: ${error}`);
